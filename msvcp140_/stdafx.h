889efe3216a2f555dd1c2ad253d7ecb04cfeb3a6
sonyps5201314 <398063392@qq.com>
2018-01-07 23:48:27 +0800

    基于ucrt及msvcrt发布一个最终可用的版本

--- msvcp140_/stdafx.h
+++ msvcp140_/stdafx.h
@@ -16,4 +16,5 @@
 
 
 // TODO: 在此处引用程序需要的其他头文件
+#include <stdio.h>
 
--- msvcr140_/crt_function.c
+++ msvcr140_/crt_function.c
@@ -1,12 +1,5 @@
 #include "stdafx.h"
 
-#pragma function(_abs64)
-
-__int64 __cdecl _abs64(__int64 num)
-{
-	return (num >= 0 ? num : -num);
-}
-
 char *(__cdecl* Real__unDName)(char *outputString, const char *name, int maxStringLength, void *(__cdecl *pAlloc)(unsigned int), void(__cdecl *pFree)(void *), unsigned __int16 disableFlags);
 char *__cdecl __unDName(char *outputString, const char *name, int maxStringLength, void *(__cdecl *pAlloc)(unsigned int), void(__cdecl *pFree)(void *), unsigned __int16 disableFlags)
 {
